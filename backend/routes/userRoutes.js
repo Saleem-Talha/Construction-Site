023d8c585486b6a49bc073787193415fbf00d850
Saleem Talha <saleemtalha967@gmail.com>
2024-05-11 06:43:30 -0700

    Commit -3

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,11 +3,15 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 
-// Define routes for user operations
+// Define routes for user CRUD operations
 router.post('/', userController.createUser);
 router.get('/', userController.getUsers);
 router.get('/:id', userController.getUserById);
 router.put('/:id', userController.updateUserById);
 router.delete('/:id', userController.deleteUserById);
+
+// Define routes for user authentication
+router.post('/signup', userController.signup);
+router.post('/signin', userController.signin);
 
 module.exports = router;
